c21acf8ec776a8ec3b07f5cd1ce9a95987a018e1
LohithMarneni <lohith.marneni@gmail.com>
2024-10-29 15:39:59 +0530

    added env variables

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,17 @@ import userRouter from "./view/userRouter.js";
 import contactMeRouter from "./view/contactMeRouter.js";
 import { errorMiddleWare } from "./middleware.js/error.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(cors(
+    {
+        origin:[process.env.FRONTEND_URL],
+        methods:["GET","POST","PUT","DELETE","PATCH"],
+        credentials:true,
+    }
+))
 app.use('/api/v1',contactMeRouter);
 app.use('/api/v1/user',userRouter);
 app.use(errorMiddleWare);
