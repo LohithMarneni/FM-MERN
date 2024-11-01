52112bc6ab3f051e0bd70d24c99dcc209091e71b
LohithMarneni <lohith.marneni@gmail.com>
2024-11-02 00:55:56 +0530

    commits till 01-11-2024

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,3 @@
-next u have to change the api call so that it remembers the previous questions and answers and based on that it replys okay
+next u have to change the api call so that it remembers the previous questions and answers and based on that it replys okay-completed
+Till now for backend u have only written to give suggestion i future u should have to return places also for suggestion based on user location
+02-11-2024: updated front end and checked backend whether it was functioning correctly or not 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from 'react'
+import backgroundImage from './assets/backgroundImage.jpg';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>This is FrontEnd of FM project</h1>
+    {/* <h1 className="bg-slate-700">This is FrontEnd of FM project(This line is for Debugging)</h1>  */}
+    <div
+     className="bg-fixed bg-cover bg-center min-h-screen"
+     style={{
+      backgroundImage: `url(${backgroundImage})`,
+     }}
+    >
+    <div className="absolute inset-0 bg-black opacity-40"></div>
+    <nav className="bg-green-100 bg-opacity-75 p-5">
+        <ul className="flex justify-between text-orange-600 text-lg font-mono font-bold">
+          <li className="mr-4"><a className="relative hover:border-b-4 hover:border-white transition duration-600" href="#Home">Home</a></li>
+          <li className="mr-4"><a className="relative hover:border-b-4 hover:border-white transition duration-600" href="#About">About</a></li>
+          <li className="mr-4"><a className="relative hover:border-b-4 hover:border-white transition duration-600" href="#Contact Us">Contact Us</a></li>
+          <li className="mr-4"><a className="relative hover:border-b-4 hover:border-white transition duration-600" href="#Login/Signup">Login/Signup</a></li>
+        </ul>
+    </nav>
+    </div>
     </>
   )
 }
